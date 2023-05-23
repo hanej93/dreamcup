4ad8e583fcb783cdab16fb98f74d0f378ea2cf98
EJ <hanej93@naver.com>
2023-05-23 10:12:57 +0900

    :tada: begin a project

---
+++ src/main/java/com/dreamcup/DreamcupApplication.java
@@ -0,0 +1,13 @@
+package com.dreamcup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamcupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamcupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dreamcup/DreamcupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamcup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamcupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
