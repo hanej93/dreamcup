2c172135da000f70191c33a87728afb7849b418f
EJ <hanej93@naver.com>
2023-05-24 15:22:17 +0900

    ✅ 회원가입 컨트롤러 테스트 추가

---
+++ src/test/java/com/dreamcup/member/controller/MemberControllerTest.java
@@ -0,0 +1,58 @@
+package com.dreamcup.member.controller;
+
+import static org.springframework.http.MediaType.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.dreamcup.member.dto.request.MemberSignupRequestDto;
+import com.dreamcup.member.repository.MemberRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class MemberControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Autowired
+	private MemberRepository memberRepository;
+
+	@BeforeEach
+	public void clean() throws Exception {
+		memberRepository.deleteAll();
+	}
+
+	@Test
+	@DisplayName("회원가입")
+	void signup() throws Exception {
+		// given
+		MemberSignupRequestDto requestDto = MemberSignupRequestDto.builder()
+			.username("user")
+			.password("1234")
+			.nickname("user-nick")
+			.build();
+
+		String request = objectMapper.writeValueAsString(requestDto);
+
+		// expected
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/signup")
+				.contentType(APPLICATION_JSON)
+				.content(request)
+			)
+			.andExpect(status().isCreated())
+			.andDo(print());
+	}
+}
