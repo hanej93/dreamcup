2b47f572f848e543a61857cfe6004d88976b5b2f
EJ <hanej93@naver.com>
2023-05-29 14:17:31 +0900

    🏷️ 불필요 패키지 경로 삭제

--- src/main/java/com/dreamcup/config/auth/CustomUserDetailsService.java
+++ src/main/java/com/dreamcup/config/auth/CustomUserDetailsService.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -30,15 +31,15 @@ public class CustomUserDetailsService implements UserDetailsService {
 			.orElseThrow(() -> new UsernameNotFoundException(username + " -> 데이터베이스에서 찾을 수 없습니다."));
 	}
 
-	private org.springframework.security.core.userdetails.User createUser(String username, Member member) {
+	private User createUser(String username, Member member) {
 		if (!member.isActivated()) {
 			throw new RuntimeException(username + " -> 활성화되어 있지 않습니다.");
 		}
 
 		List<GrantedAuthority> grantedAuthorities = member.getAuthorities().stream()
-			.map(authority -> new SimpleGrantedAuthority(authority.getId().getAuthority().toString()))
+			.map(authority -> new SimpleGrantedAuthority(authority.getId().getAuthority().name()))
 			.collect(Collectors.toList());
 
-		return new org.springframework.security.core.userdetails.User(member.getUsername(), member.getPassword(), grantedAuthorities);
+		return new User(member.getUsername(), member.getPassword(), grantedAuthorities);
 	}
 }
