9993b04fc3cd5a5c450208d3f32aff33066f7612
ejhan <hanej93@naver.com>
2024-02-07 14:40:59 +0900

    🦺 Add Validation Enum Config

---
+++ src/main/java/com/dreamcup/common/validator/EnumValidator.java
@@ -0,0 +1,30 @@
+package com.dreamcup.common.validator;
+
+
+import com.dreamcup.common.annotation.Enum;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class EnumValidator implements ConstraintValidator<Enum, String> {
+
+    private Enum annotation;
+
+    @Override
+    public void initialize(Enum constraintAnnotation) {
+        this.annotation = constraintAnnotation;
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        Object[] enumValues = this.annotation.enumClass().getEnumConstants();
+        if (enumValues != null) {
+            for (Object enumValue : enumValues) {
+                if (value.equals(enumValue.toString())
+                        || (this.annotation.ignoreCase() && value.equalsIgnoreCase(enumValue.toString()))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
